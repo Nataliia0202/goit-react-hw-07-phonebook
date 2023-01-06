1fbb44cbfe60e790c5b174e0266add0d62177095
Nataliia Svystula <natashasvistula@gmail.com>
2023-01-06 14:28:04 +0200

    add

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -7,28 +7,23 @@ import {
 import { ReactComponent as DeletIcon } from '../Icon/remove-user.svg';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { getContacts, getFilter } from 'redux/selector';
+// import { getContacts, getFilter } from 'redux/selector';
 import { deleteContact } from 'redux/operations';
-
+import { selectFilteredContacts } from 'redux/selector';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter).toLowerCase();
-
-  const doFiltering = () =>
-    contacts.filter(contact => contact.name.toLowerCase().includes(filter));
-  const filteredContacts = doFiltering();
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <div>
       <ContactListStyle>
-        {filteredContacts.map((contact, id) => (
-          <ContactItem key={id}>
+        {filteredContacts.map((contact) => (
+          <ContactItem key={contact.id}>
             {contact.name}: {contact.number}
             <ButtonForDelete
               type="button"
-              onClick={() => dispatch(deleteContact(id))}
+              onClick={() => dispatch(deleteContact(contact.id))}
             >
               <DeletIcon width="32" heigth="32" />
             </ButtonForDelete>
